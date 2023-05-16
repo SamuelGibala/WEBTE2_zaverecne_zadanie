cdc34d4bea4e2abc5206b477ded53ee716d60e53
SamuelGibala <samko.gibala@gmail.com>
2023-05-16 20:38:35 +0200

    fix: info modal

--- info_modal.php
+++ info_modal.php
@@ -1,4 +1,4 @@
-é=´9<?php
+<?php
 function add_info_modal_btn() {
     echo 
     '<button type="button" class="btn btn-secondary btn-info-modal me-3" data-bs-toggle="modal" data-bs-target="#infoModal">
@@ -16,15 +16,36 @@ function add_info_modal() {
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="infoModalTitle">
-                        Návod na používanie webovej stránky
+                        Návod na používanie aplikácie
                     </h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" id="infoModalBody">
-                    1.riadok sdnadad sdnasoidhaso sadhasodaso sdhasod sadhaod<br>
-                    2.riadok gfdg sdad<br>
-                    3.riadok gdfg sdasd dasd sdasd hhhhhhhhh ssdsd hhhhhhhhhhhhhhsd hhhhhhhhhhhhhsda hhhhhhhhhhhhhasd <br>
-                    4.riadok dasd sdasd asdasda sdad asdaddfg<br>
+                    <h2 class="modal-title fs-5">Študent</h2>
+                    <ul>
+                        <li>Po prihlásení je možné v menu vľavo vybrať medzi priradenými úlohami na vypracovanie.</li>
+                        <li>Pri výbere priradených úloh sú zobrazené aktuálne dostupné úlohy.</li>
+                        <li>Z nich si študent môže v prvom kroku vygenerovať úlohu v prvej časti stránky s názvom "Dostupné testy".</li>
+                        <li>Pri generovaní študent môže označiť minimálne jeden súbor, z ktorého/ktorých bude úloha generovaná</li>
+                        <li>Po potvrdení študent presmerovaný naspäť na stránku priradených úloh, kde môže danú vygenerovanú úlohu vypracovať v druhej časti stránky s názvom "Testy na vypracovanie"</li>
+                        <li>Po kliknutí na tlačidlo "Vypracovať" je zobrazené zadanie úlohy a v časti "Tvoje riešenie" má študent možnosť zadať riešenie zadania pomocou matematického editora. Pri ňom sa po kliknutí na symbol klávesnice zobrazí virtuálna klávesnica so všetkými potrebnými matematickými operáciami a znakmi.</li>
+                        <li>Po stlačení tlačidla "Odoslať" je študent presmerovaný na stránku "Vypracované úlohy", kde má možnosť prehliadať už vypracované testy spolu so získaným ohodnotením.</li>
+                        <li>Po kliknutí na tlačidlo "Nahliadnuť" sa študentovi zobrazí náhľad vypracovanej úlohy spolu so zadaným a správnym riešením</li>
+                        <li>V hornej časti aplikácie sa nachádza sprava tlačidlo na odhlásenie, identifikátor prihláseného používateľa, rozbaľovacie menu na zmenu jazyka a informačné tlačidlo pre zobrazenie tohto dokumentu s možnosťou tlače do PDF</li>
+                    </ul>
+                    
+                    <h2 class="modal-title fs-5">Učiteľ</h2>
+                    <ul>
+                        <li>Po prihlásení je užívateľ presmerovaný do aplikácie konkrétne do nástroja na generovanie úloh.</li>
+                        <li>Tu je možné po vyplnení formulára vytvoriť úlohu pre študentov s alebo bez dátumov začiatku platnosti, s bodovým ohodnotením a z vybraných dokumentov</li>
+                        <li>Po vygenerovaní je používateľ presmerovaný na zoznam vygenerovaných úloh</li>
+                        <li>Ďalej má používateľ možnosť prehliadať zoznam študentov. Po kliknutí na študenta je zobrazený detail jeho profilu spolu s vypracovanými úlohami</li>
+                        <li>Po kliknutí na tlačidlo "Nahliadnuť" má používateľ možnosť nahliadnuť do odovzdaného testu, kde okrem iného vidí zadaný výsledok študenta</li>
+                        <li>Tabuľku študentov je možné exportovať kliknutím na tlačidlo "Stiahni ako CSV" do csv formátu</li>
+                        <li>Učiteľ, ako správca aplikácie má možnosť pridať ďalšieho poúžívateľa, študednta alebo učiteľa</li>
+                        <li>V hornej časti aplikácie sa nachádza sprava tlačidlo na odhlásenie, identifikátor prihláseného používateľa, rozbaľovacie menu na zmenu jazyka a informačné tlačidlo pre zobrazenie tohto dokumentu s možnosťou tlače do PDF</li>
+                        <li style="color: red">Pri pridaní nového testu je potrebné dodržať štruktúru podľa už existujúcich LaTex dokumentov, rovnako ako pri prípadnej grafickej podobe zadania nastaviť správne cesty k obrázku podľa príslušnej použitej architektúry!</li>
+                    </ul>
                 </div>
                 <div class="modal-footer">
                     <button type="button" id="export_pdf_btn" class="btn btn-danger">' . get_localized('download_pdf') . '</button>
