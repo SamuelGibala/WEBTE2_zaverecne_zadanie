28411bc899811f4d0106d42097cec2102f199891
SamuelGibala <samko.gibala@gmail.com>
2023-05-16 19:46:56 +0200

    path: logo+responsive

--- homeS.php
+++ homeS.php
@@ -39,13 +39,14 @@ $incompleted = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!doctype html>
 <html lang="sk">
 <head>
-    <title>Home Page</title>
+    <title>Student home</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/responsive.css">
 </head>
 <body>
 <header>
@@ -99,15 +100,13 @@ $incompleted = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <a class="navbar-brand" href="#">
                 <img
                         src="https://am2023.sski.sk/wp-content/uploads/2023/01/feistu.png"
-                        height="40"
-                        alt=""
+                        alt="FEI logo"
                         loading="lazy"
+                        class="logos"
                 />
             </a>
 
-            <div style="margin: 0 auto"><?php echo get_localized('menu_header') ?></div>
-
-
+            <div style="margin: 0 auto"> <img src="flags/logo-no-background.png" class="logos" alt="e-FEIster"></div>
 
             <!-- Right links -->
             <ul class="navbar-nav d-flex flex-row">
